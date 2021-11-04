5562ddccc59652a33d74e724aadd2e0e2fda0a26
rashidRizvi98 <rashid123321a@gmail.com>
2021-11-04 22:32:27 +0530

    init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static Logger logger= LoggerFactory.getLogger(DemoApplication.class);
+
+   @PostConstruct
+   public void init(){
+       logger.info("App started");
+   }
+
+    public static void main(String[] args) {
+        logger.info("App mainmethod executed");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    public static Logger logger= LoggerFactory.getLogger(DemoApplication.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Testcase executed");
+        assertEquals(true,true);
+    }
+
+}
